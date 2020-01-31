a005be01d1c26c19e73243b6d77fb6eb39932228
Bruno Artur <batllopes@yahoo.com.br>
2020-01-31 14:18:31 -0300

    Views criadas
    Foram Criadas três views:
    
    * dental-list -> Recebe os dados consultados do banco com o nome "data", e exibe uma lista com esses dados.
    
    * dental-register -> Contém um formulário para fazer os registros.
    
    * dental-register-success -> É a view "dentall-register" acrecida de uma mensagem de sucesso.

---
+++ resources/views/dental-list.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Dentistas</title>
+
+        <!-- Fonts -->
+        <link href="css/app.css" rel="stylesheet">
+
+        <!-- Styles -->
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="nav-link" href="{{url('/register')}}">Registro de Dentista</a>
+            </div>
+        </nav>
+        <div class="container" style="margin-top: 50px;">
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th style="min-width: 170px;">Nome</th>
+                        <th>Consultório</th>
+                        <th>Dia</th>
+                        <th>Horário</th>
+                    </tr>
+                </thead>
+                <tbody class="table-striped">
+                @foreach ($data as $d)
+                    <tr>
+                        <th style="text-align: center; vertical-align: middle;" rowspan="{{count($d->clinic)}}">{{$d->name}}</th>
+                        @foreach($d->clinic as $c)
+                            <td>{{$c->name}}</td>
+                            <td>
+                                @php
+                                    $i = count($d->clinic[0]->pivot->days->days);
+                                    foreach($d->clinic[0]->pivot->days->days as $day){
+                                        $i--;
+                                        if($i==0){
+                                            echo $day;
+                                        } else{
+                                            echo $day.", ";
+                                        }
+                                    }
+                                @endphp
+                            </td>
+                            <td>
+                                @php
+                                    $time = strtotime($d->clinic[0]->pivot->start_time);
+                                    echo date("H:i", $time);
+                                @endphp
+                                -
+                                @php
+                                    $time = strtotime($d->clinic[0]->pivot->end_time);
+                                    echo date("H:i", $time);
+                                @endphp
+                            </td>
+                    </tr>
+                @endforeach
+                        @endforeach
+
+                </tbody>
+            </table>
+        </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/dental-register-success.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Registro</title>
+
+        <!-- Fonts -->
+        <link href="css/app.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="nav-link" href="{{url('/')}}">&#8656 Lista de Dentistas</a>
+            </div>
+        </nav>
+
+        <div class="container" style="margin-top: 50px;">
+            <div class="alert alert-success" >
+                Dentista Cadastrado com Sucesso!!!
+            </div>
+            <form action="" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="dentistName">Nome: </label>
+                    <input class="form-control" type="text" name="dentistName" id="dentistName">
+                </div>
+                <div class="form-group">
+                    <label for="clinicName">Clínica</label>
+                    <input class="form-control" type="text" name="clinicName" id="clinicName">
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="days[]" value="seg" id="seg">
+                    <label for="seg">Segunda-Feira</label><br>
+                    <input type="checkbox" name="days[]" value="ter" id="ter">
+                    <label for="ter">Terça-Feira</label><br>
+                    <input type="checkbox" name="days[]" value="qua" id="qua">
+                    <label for="qua">Quarta-Feira</label><br>
+                    <input type="checkbox" name="days[]" value="qui" id="qui">
+                    <label for="qui">Quinta-Feira</label><br>
+                    <input type="checkbox" name="days[]" value="sex" id="sex">
+                    <label for="sex">Sexta-Feira</label><br>
+                    <input type="checkbox" name="days[]" value="sab" id="sab">
+                    <label for="sab">Sábado-Feira</label><br>
+                </div>
+                <div class="form-group">
+                    <label for="startTime">Início</label>
+                    <input type="time" name="start_time" id="startTime">
+                    <label for="endTime">Fim</label>
+                    <input type="time" name="end_time" id="endTime">
+                </div>
+
+                <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
+            </form>
+        </div>
+    <link href="js/app.js" type="text/javascript">
+    </body>
+</html>
